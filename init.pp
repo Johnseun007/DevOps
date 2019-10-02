288df0cde1e760e76c9fe79f09f6e81fae1dc7be
root <root@ip-172-31-0-48.us-east-2.compute.internal>
2019-10-02 16:46:35 +0000

    add puppet files

---
+++ init.pp
@@ -0,0 +1,37 @@
+class webserver(
+                $packagename = hiera('packagename'),
+                $configfile = hiera('configfile'),
+                $configsource = hiera('configsource'),
+                $vhostfile = hiera('vhostfile')
+                ) {
+
+        package { 'webserver-package':
+                name   => $packagename,
+                ensure => present
+        }
+
+        file { 'config-file':
+                path   => $configfile,
+                ensure => file,
+                source => $configsource,
+                require => Package['webserver-package'],
+                notify  => Service['webserver-service'],
+        }
+
+        file { 'vhost-file':
+                path    => $vhostfile,
+                ensure  => file,
+                content => template('webserver/vhost.conf.erb'),
+                require => Package['webserver-package'],
+                notify  => Service['webserver-service'],
+        }
+
+        service { 'webserver-service':
+                name        => $packagename,
+                ensure      => running,
+                enable      => true,
+                hasrestart  => true,
+                require     => [ File['config-file'], File['vhost-file'] ],
+        }
+}
+
